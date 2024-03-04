e9366facdf52293ce5e5390a449c6f4fc25764a0
hhhwrxhhh <hhhwrxhhh@gmail.com>
2024-03-04 17:30:44 +0200

    finished multiwindow

--- multiwindow_tunnel/src/script.js
+++ multiwindow_tunnel/src/script.js
@@ -17,18 +17,20 @@ export default class Sketch {
     document.getElementById('container').appendChild(this.renderer.domElement);
 
     this.camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.01, 10);
-    this.camera.position.set(0, 0.0, 0.1);
+    this.camera.position.set(0, 0.0, 0);
+    this.camera.lookAt(0, 0.0, -100);
 
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color( 0xffffff );
+    this.scene.background = new THREE.Color( 0x000000 );
     this.clock = new THREE.Clock();
 
-    this.numPlanes = 90;
-    this.meshes = []
-    this.materials = []
+    this.numPlanes = 120;
+    this.meshes = [];
+    this.materials = [];
+    this.target = new THREE.Vector2(0, 0);
 
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.enableDamping = true; 
+    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    // this.controls.enableDamping = true; 
 
     this.addMesh();
 
@@ -61,26 +63,27 @@ export default class Sketch {
       let material = getMaterial(i / 30);
       let tmpMesh = new THREE.Mesh(geometry, material);
 
-      tmpMesh.position.z =  -0.1 * i; 
+      tmpMesh.position.z =  -0.2 * i; 
       this.scene.add(tmpMesh);
       this.meshes.push(tmpMesh);
       this.materials.push(material)
     }
   }
 
-
-
   render() {
     const elapsedTime = this.clock.getElapsedTime();
 
-    // this.mesh.rotation.x = Math.sin(elapsedTime);
-    // this.mesh.rotation.y = Math.cos(elapsedTime);
-
+    this.camera.position.z = -elapsedTime * 0.1; 
     this.materials.forEach((material, i) => {
-
+      material.uniforms.uTime.value = elapsedTime * 0.5;
     })
 
-    this.controls.update();
+    // here is magic
+    this.target.lerp(new THREE.Vector2(window.screenLeft, window.screenTop), 0.1);
+    this.camera.setViewOffset(window.screen.width, window.screen.height, 
+      this.target.x, this.target.y, window.innerWidth, window.innerHeight);
+   
+      // this.controls.update();
     this.renderer.render(this.scene, this.camera);
 
     window.requestAnimationFrame(this.render.bind(this));
--- multiwindow_tunnel/src/shaders/vertex.glsl
+++ multiwindow_tunnel/src/shaders/vertex.glsl
@@ -1,6 +1,9 @@
 varying vec2 vUv;
+varying vec3 vWorldPosition;
 
 void main() {
+    vWorldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
+
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
     vUv = uv;
 }
